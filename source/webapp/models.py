cf42881f2854e1ae040452df3337ca64ca0c6b7f
Alexandr <nuvorish81@gmail.com>
2020-08-13 18:32:34 +0600

    сделанны миграции обнавлен requrements.txt

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -17,12 +17,11 @@ def is_null(string):
 
 
 class IssueTracker(models.Model):
-    summary = models.CharField(max_length=300, null=False, blank=False, default="None description", verbose_name='Задание',
-                               validators=[is_title,])
-    description = models.TextField(max_length=3500, null=True, blank=False, default="None description", verbose_name='Описание',
+    summary = models.CharField(max_length=300, null=False, blank=False, default="None", verbose_name='Задание',
+                               validators=[is_title, ])
+    description = models.TextField(max_length=3500, null=True, blank=True, default="None description", verbose_name='Описание',
                                    validators=[is_null, ])
     status = models.ForeignKey('webapp.Status', related_name='issue', on_delete=models.PROTECT, verbose_name='Статус')
-    # type_old = models.ForeignKey('webapp.Type', related_name='type_set', on_delete=models.PROTECT, verbose_name='Тип')
     type = models.ManyToManyField('webapp.Type', related_name='type', verbose_name='Тип')
     completion_time = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
 
