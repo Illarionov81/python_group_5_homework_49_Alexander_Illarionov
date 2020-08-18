2f5a3859b4b77450c9ad120fd9302d42e229eba7
Alexandr <nuvorish81@gmail.com>
2020-08-18 22:52:38 +0600

    главная страница переделана на ListView + добавленна пагинация

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -2,17 +2,28 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from webapp.models import IssueTracker
 from webapp.forms import TaskForm
-from django.views.generic import View, TemplateView, FormView
+from django.views.generic import View, TemplateView, FormView, ListView
 
 
-class TasksView(TemplateView):
+class TasksView(ListView):
     template_name = 'index.html'
+    context_object_name = 'task_list'
+    model = IssueTracker
+    paginate_by = 10
+    paginate_orphans = 1
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        task_list = IssueTracker.objects.all()
-        context['task_list'] = task_list
-        return context
+    def get_queryset(self):
+        return IssueTracker.objects.all().order_by('-completion_time')
+
+
+# class TasksView(TemplateView):
+#     template_name = 'index.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         task_list = IssueTracker.objects.all()
+#         context['task_list'] = task_list
+#         return context
 
 
 class OneTaskView(TemplateView):
