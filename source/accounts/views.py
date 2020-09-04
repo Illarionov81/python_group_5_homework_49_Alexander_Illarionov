e2304f1fe7c6df5db8e12cc84f38a1a382f9035b
Alexandr <nuvorish81@gmail.com>
2020-09-04 22:45:52 +0600

    сделанна форма модели users и view регистрации + html - простая не базовая

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from accounts.forms import MyUserCreationForm
 
 
 # def login_view(request):
@@ -16,8 +17,18 @@ from django.shortcuts import render, redirect
 #             context['has_error'] = True
 #     return render(request, 'registration/login.html', context=context)
 #
-#
 # def logout_view(request):
 #     from_url = request.META.get('HTTP_REFERER', 'products')
 #     logout(request)
 #     return redirect(from_url)
+
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = MyUserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = MyUserCreationForm()
+    return render(request, 'user_create.html', context={'form': form})
