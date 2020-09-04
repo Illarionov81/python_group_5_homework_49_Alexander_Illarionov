d971761097f21cd4232c7ad1dd5a596a02fcf6cb
Alexandr <nuvorish81@gmail.com>
2020-09-05 00:11:13 +0600

    страница регистрации переделанна на встроенную форму регистрации и представление на CreateView

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from accounts.forms import MyUserCreationForm
+from  django.views.generic import CreateView
 
 
 # def login_view(request):
@@ -22,13 +26,21 @@ from accounts.forms import MyUserCreationForm
 #     logout(request)
 #     return redirect(from_url)
 
-def register_view(request, *args, **kwargs):
-    if request.method == 'POST':
-        form = MyUserCreationForm(data=request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('index')
-    else:
-        form = MyUserCreationForm()
-    return render(request, 'user_create.html', context={'form': form})
+
+class RegisterView(CreateView):
+    model = User
+    template_name = 'user_create.html'
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('index')
+        return next_url
