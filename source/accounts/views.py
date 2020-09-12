5357d3107cd3c88ded7f39226be2922c2e95aacb
Alexandr <nuvorish81@gmail.com>
2020-09-12 22:44:11 +0600

    Добавлена личная страница пользователя

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -2,6 +2,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.views.generic import DetailView
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 from accounts.forms import MyUserCreationForm
 from  django.views.generic import CreateView
@@ -44,3 +48,21 @@ class RegisterView(CreateView):
         if not next_url:
             next_url = reverse('index')
         return next_url
+
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+    # paginate_related_by = 5
+    # paginate_related_orphans = 0
+
+    # def get_context_data(self, **kwargs):
+    #     project = self.object.project
+    #     paginator = Paginator(project, self.paginate_related_by, orphans=self.paginate_related_orphans)
+    #     page_number = self.request.GET.get('page', 1)
+    #     page = paginator.get_page(page_number)
+    #     kwargs['page_obj'] = page
+    #     kwargs['project'] = page.object_list
+    #     kwargs['is_paginated'] = page.has_other_pages()
+    #     return super().get_context_data(**kwargs)
